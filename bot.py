87fe0953fc57738446ac59f3c86b376a79cef47c
Sofía Denner <sofia.denner@mercadolibre.com>
2021-10-03 11:52:49 -0300

    first edition of bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,6 @@
-import logging
-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from local_settings import BOT_TOKEN
-
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
-updater = Updater(token=BOT_TOKEN)
-
-dispatcher = updater.dispatcher
 
 
 def start(update, context):
@@ -21,36 +9,25 @@ def start(update, context):
         text="I'm a bot, please talk to me!"
     )
 
+start_handler = CommandHandler('start', start)
+
+
 def echo(update, context):
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=update.message.text
     )
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
 
 
-from telegram import InlineQueryResultArticle, InputTextMessageContent
-def inline_caps(update, context):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    context.bot.answer_inline_query(update.inline_query.id, results)
+if __name__ == "__main__":
+    updater = Updater(token=BOT_TOKEN)
 
-from telegram.ext import InlineQueryHandler
-inline_caps_handler = InlineQueryHandler(inline_caps)
-dispatcher.add_handler(inline_caps_handler)
+    dispatcher = updater.dispatcher
 
-updater.start_polling()
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(echo_handler)
+
+    updater.start_polling()
