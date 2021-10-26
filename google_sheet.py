347ce33ce87e815e74231fdb15f3a71e887ccbd0
Sofía Denner <sofia.denner@mercadolibre.com>
2021-10-25 21:39:38 -0300

    ignore google sheet error for now

--- google_sheet.py
+++ google_sheet.py
@@ -11,11 +11,11 @@ scope = [
     'https://www.googleapis.com/auth/drive.file'
     ]
 key_filename = 'sheet_client_key.json'
-creds = ServiceAccountCredentials.from_json_keyfile_name(key_filename, scope)
-client = gspread.authorize(creds)
-
-FEEDBACK_SHEET_NAME = "Telegram feedback"
-FEEDBACK_SHEET = client.open(FEEDBACK_SHEET_NAME).sheet1
+# creds = ServiceAccountCredentials.from_json_keyfile_name(key_filename, scope)
+# client = gspread.authorize(creds)
+#
+# FEEDBACK_SHEET_NAME = "Telegram feedback"
+# FEEDBACK_SHEET = client.open(FEEDBACK_SHEET_NAME).sheet1
 
 
 def add_feedback(feedback, username, full_name):
