185e5e3ff16c577dca9e2036b072a7c3cdd7fa9e
Sofía Denner <sofia.denner@mercadolibre.com>
2021-10-25 22:19:24 -0300

    remove old code

--- flask_app.py
+++ flask_app.py
@@ -8,27 +8,6 @@ from settings import BOT_TOKEN, SITE_DOMAIN, DEBUG
 
 app = Flask(__name__)
 bot = Bot(BOT_TOKEN)
-WEBHOOK_SETTED = False
-
-
-def set_webhook():
-    global WEBHOOK_SETTED
-    if not DEBUG and not WEBHOOK_SETTED:
-        webhook_endpoint = f"{SITE_DOMAIN}/{BOT_TOKEN}"
-        print("about to sed webhook")
-        print("endpoint:", webhook_endpoint)
-        bot.set_webhook(webhook_endpoint)
-        WEBHOOK_SETTED = True
-        print("webhook setted")
-
-
-if not DEBUG:
-    print("not debug, setting webhook")
-    webhook_endpoint = f"{SITE_DOMAIN}/{BOT_TOKEN}"
-    print("webhook endpoint", webhook_endpoint)
-    bot.set_webhook(webhook_endpoint)
-    print("webhook ready")
-
 
 dispatcher = Dispatcher(bot, None)
 add_all_handlers(dispatcher)
@@ -42,7 +21,6 @@ def index():
 def bot_response():
     print("im in bot response!!")
     update = request.get_json()
-    # set_webhook()
     dispatcher.process_update(Update.de_json(update, bot))
 
     print("im in bot response!! - before return")
